a6da5e29a6925086d5c92125720c572282b038b5
Ashwin Rajesh <ashwinr2k2@gmail.com>
2020-03-23 22:03:52 +1100

    working test function and pygame as requirement

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name="pgscript", version="0.1", description="high level objects for pygame", author="Ashwin Rajesh", packages=['pgscript'], zip_safe=False, install_requires=['pygame'])
