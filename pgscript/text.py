9937077e0c315c51f3b88369251a5a491de8a8df
Ashwin Rajesh <ashwinr2k2@gmail.com>
2020-06-09 07:44:28 +1000

    text surf

--- pgscript/text.py
+++ pgscript/text.py
@@ -1,7 +1,20 @@
 import pygame
+import threading
 
 pygame.font.init()
 
-class message():
-    def __init__(self, DISPLAY, text):
+
+def draw_surface(DISPLAY, surface, pos, frames=1):
+    for n in frames:
+        DISPLAY.blit(surface, pos)
+
+
+
+def message(DISPLAY, text, font, color, pos, frames):
+    text_surf = font.render(text, True, color)
+    msg_handler = threading.Thread(target=draw_surface, args=(DISPLAY, text_surf, pos, frames,)) 
+    msg_handler.start()
+
+class text():
+    def __init__(self, DISPLAY):
         pass
